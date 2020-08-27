998bb792d07e0da204dffd1d8772c49597335f32
priyamkhatri24 <68660957+priyamkhatri24@users.noreply.github.com>
2020-08-27 17:26:35 +0530

    pages updated

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49334" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50057" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
